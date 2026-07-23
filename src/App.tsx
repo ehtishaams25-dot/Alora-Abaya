965c65162bbcb3d8656e66ab71562f84882e1403
ehtishaams25-dot <ehtishaams25@gmail.com>
2026-07-24 03:39:22 +0530

    feat: implement Lenis smooth scrolling and define main App routing structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { useEffect } from 'react'
 import { Routes, Route, useLocation } from 'react-router-dom'
+import { ReactLenis } from 'lenis/react'
 import { HomePage } from './pages/HomePage'
 import { LoginPage } from './pages/LoginPage'
 import { WishlistPage } from './pages/WishlistPage'
@@ -51,32 +52,34 @@ function GlobalSizeGuideContainer() {
 
 export function App() {
   return (
-    <QueryProvider>
-      <ShopProvider>
-        <ScrollToHashElement />
-        <CartDrawerModal />
-        <GuestCheckoutModal />
-        <GlobalQuickViewContainer />
-        <GlobalSizeGuideContainer />
-        <SearchOverlayModal />
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/dresses" element={<AllDressesPage />} />
-          <Route path="/all-dresses" element={<AllDressesPage />} />
-          <Route path="/shop" element={<AllDressesPage />} />
-          <Route path="/product/:id" element={<ProductPage />} />
-          <Route path="/dresses/:id" element={<ProductPage />} />
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/wishlist" element={<WishlistPage />} />
-          <Route path="/bag" element={<CartPage />} />
-          <Route path="/cart" element={<CartPage />} />
-          <Route path="/account" element={<AccountDashboardPage />} />
-          <Route path="/profile" element={<AccountDashboardPage />} />
-          <Route path="/dashboard" element={<AccountDashboardPage />} />
-          <Route path="/orders" element={<AccountDashboardPage />} />
-        </Routes>
-      </ShopProvider>
-    </QueryProvider>
+    <ReactLenis root options={{ lerp: 0.1, duration: 1.2, smoothWheel: true }}>
+      <QueryProvider>
+        <ShopProvider>
+          <ScrollToHashElement />
+          <CartDrawerModal />
+          <GuestCheckoutModal />
+          <GlobalQuickViewContainer />
+          <GlobalSizeGuideContainer />
+          <SearchOverlayModal />
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/dresses" element={<AllDressesPage />} />
+            <Route path="/all-dresses" element={<AllDressesPage />} />
+            <Route path="/shop" element={<AllDressesPage />} />
+            <Route path="/product/:id" element={<ProductPage />} />
+            <Route path="/dresses/:id" element={<ProductPage />} />
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/wishlist" element={<WishlistPage />} />
+            <Route path="/bag" element={<CartPage />} />
+            <Route path="/cart" element={<CartPage />} />
+            <Route path="/account" element={<AccountDashboardPage />} />
+            <Route path="/profile" element={<AccountDashboardPage />} />
+            <Route path="/dashboard" element={<AccountDashboardPage />} />
+            <Route path="/orders" element={<AccountDashboardPage />} />
+          </Routes>
+        </ShopProvider>
+      </QueryProvider>
+    </ReactLenis>
   )
 }
 
