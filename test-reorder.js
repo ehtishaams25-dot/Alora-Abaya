ac213911adc21150341596ed859346836dd93d83
Gulam-EvoTech <Gulam@evotech.global>
2026-07-24 11:55:47 +0530

    Remove various check and test scripts related to node and page inspection in the Framer project.

--- test-reorder.js
+++
@@ -1,5 +0,0 @@
-async function checkProto() {
-  console.log("agent proto methods:", Object.getOwnPropertyNames(Object.getPrototypeOf(framer.agent)));
-  console.log("framer proto methods:", Object.getOwnPropertyNames(Object.getPrototypeOf(framer)));
-}
-return await checkProto();
--- test-serialize.js
+++
@@ -1,6 +0,0 @@
-async function testSerializeDSL() {
-  const d = await framer.agent.serialize({ id: "TDz01QmQg" });
-  console.log("serialize({id}) type:", typeof d);
-  console.log("snippet:", typeof d === "string" ? d.substring(0, 300) : JSON.stringify(d).substring(0, 300));
-}
-return await testSerializeDSL();
